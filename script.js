fa1fb67f723b2b06d48b1563f59082e03055fb08
EducatedStrikeCart <dhn004@gmail.com>
2021-08-16 21:02:45 -0400

    added data from json

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // TODO: add code here
+
+window.addEventListener("load", () => {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+        .then((response) => response.json())
+        .then((data) => {
+            let container = document.getElementById("container")
+            for (entry in data) {
+                let astronaut = data[entry];
+                container.innerHTML += `
+                <div class="astronaut">
+                  <div class="bio">
+                    <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                    <ul>
+                      <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                      <li>Active: ${astronaut.active}</li>
+                      <li>Skills: ${astronaut.skills}</li>
+                    </ul>
+                  </div>
+                  <img class="avatar" src="${astronaut.picture}">
+                </div>
+                `
+            };
+        });
+});
